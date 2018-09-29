29776d2c5cebd3c050ed49b7a3ebdab8f8298c8d
johnpchung <john_p_chung@yahoo.com>
2018-09-29 15:05:00 -0700

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+import sys
+print(sys.executable)
+print(sys.version)
+
+print("hello,world")
+for num in [1, 2, 3, 4]:
+    print(num)
+
+print(1+7)
---
+++ main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    console.log ('Hello');
+}
+
+var c = 'test';
+var a = 'abc';
